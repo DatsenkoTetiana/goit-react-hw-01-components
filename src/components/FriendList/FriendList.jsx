0286255fa932e4a53386779551d374c732546a9d
tetianadac <datsenkotetiana027@gmail.com>
2023-01-06 23:20:37 +0200

    Update FriendList.jsx

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -3,22 +3,23 @@ import { Wrapper } from './FriendList.styled.jsx';
 import { FriendListItem } from './FriendListItem';
 
 export const FriendList = ({ friends }) => {
-    return (
-        <Wrapper>
-            {friends.map(({ id, avatar, name, isOnline }) => (
-                <FriendListItem
-                    key={id}
-                    avatar={avatar}
-                    name={name}
-                    isOnline={isOnline} />
-            ))}
-        </Wrapper>
-    );
+  return (
+    <Wrapper>
+      {friends.map(({ id, avatar, name, isOnline }) => (
+        <FriendListItem
+          key={id}
+          avatar={avatar}
+          name={name}
+          isOnline={isOnline}
+        />
+      ))}
+    </Wrapper>
+  );
 };
 FriendList.propTypes = {
-    friends: PropTypes.arrayOf(
-        PropTypes.shape({
-            id: PropTypes.number,
-        })
-    ),
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number,
+    })
+  ),
 };
