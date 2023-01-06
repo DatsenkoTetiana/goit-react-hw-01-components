1d89119737f739449991c555e6ff5b186d01e1b0
tetianadac <datsenkotetiana027@gmail.com>
2023-01-06 14:04:08 +0200

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,4 @@
+import { Profile } from "./Profile/Profile";
 export const App = () => {
   return (
     <div
---
+++ src/components/Profile/Profile.styled.jsx
@@ -0,0 +1,63 @@
+export const Profile = styled.div`
+margin: 0 auto;
+width: 350px;
+border-radius: 10px;
+background-color: #fff;
+box-shadow: 5px 6px 13px 5px #c6cccced;
+oveflow: hidden;
+`;
+export const Description = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+padding: 40px 0;
+`;
+export const Avatar = styled.img`
+height: 200px;
+width: 200px;
+background-color:  #e7eeeeed;
+border-radius: 50%;
+`;
+export const Name = styled.p`
+font-weight: 700;
+font-size: 40px;
+margin-top: 20px;
+color: gray;
+`;
+export const Tag = styled.p`
+font-weight: 500;
+font-size: 30px;
+color: gray;
+margin-top: 15px;
+`;
+export const Location = styled.p`
+font-weight: 700;
+font-size: 30px;
+color: gray;
+margin-top: 15px;
+`;
+export const Stats = styled.ul`
+display: flex;
+width: 100%;
+margin: 0;
+padding: 0;
+  justify-content: space-between;
+`;
+export const StatsItem = styled.li`
+display: flex;
+flex-direction: column;
+align-items: center;
+width: 100%;
+padding: 30px 0;
+background-color: #f0f0f0;
+`;
+export const Label = styled.span`
+font-weight: 700;
+font-size: 30px;
+color: gray;
+margin-top: 15px;
+`;
+export const Quantity = styled.span`
+font-weight: 800;
+font-size: 50px;
+`
